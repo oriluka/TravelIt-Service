d55bf6379f55816f80691d4e872856990fa755a6
Nicholas Heo <58013260+my-name-is-nheo@users.noreply.github.com>
2020-04-04 20:47:51 -0400

    Revert "Postgres"

--- server/server.js
+++ server/server.js
@@ -1,52 +1,63 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-
 const cors = require('cors');
-const port = 3005;
-const psql = require('./db/queries.js');
+const port = 3008;
+const mongoose = require('mongoose');
+const { Host, Area } = require('./models/Schema.js');
 const bodyParser = require('body-parser');
 
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.use(express.static(path.resolve(__dirname, '../public')));
 app.use(bodyParser());
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
+app.get('/area', cors(), function(req, res) {
+  Area.find(req.query, (err, arr) => res.send(arr));
+});
 
-///////// POSTGRES ENDPOINTS /////
-
-app.get('/host', (req, res) => {
-  psql.get(req.body, (err, data) => {
+app.get('/host', cors(), function(req, res) {
+  Host.find(req.query, (err, arr) =>{
     if (err) {
-      console.log('error');
       res.status(400);
       res.send(err);
     } else {
       res.status(200);
-      res.send(data);
+      res.send(arr);
     }
   });
 });
 
-app.post('/host', cors(), (req, res) => {
-  psql.post(req.body, (err, data) => {
+app.get('/app.js', cors(), function (req, res) {
+  res.sendFile(path.join(__dirname, '../public/bundle.js'))
+});
+
+app.get('/zip', cors(), function (req, res) {
+  Host.find({}, 'zip', (err, data) => res.send(data));
+});
+
+// post
+app.post('/host', cors(), function(req, res) {
+  console.log(req.body);
+  Host.create(req.body, (err, arr) => {
     if (err) {
+      console.log(err);
       res.status(400);
-      res.send(err);
     } else {
       res.status(200);
-      res.send(data);
+      res.send(arr);
     }
   });
 });
 
 // put
-app.put('/host', (req, res) => {
-  psql.put(req.body, (err, data) => {
+app.put('/host', cors(), function(req, res) {
+  Host.updateOne(req.body.query, req.body.update, (err, data) => {
     if (err) {
-      res.status(400);
-      res.send(err);
+      console.log(err);
     } else {
       res.status(200);
       res.send(data);
@@ -55,14 +66,13 @@ app.put('/host', (req, res) => {
 });
 
 // delete
-app.delete('/host', (req, res) => {
-  psql.delete(req.body, (err, data) => {
+app.delete('/host', cors(), function(req, res) {
+  Host.deleteOne(req.query, (err, data) => {
     if (err) {
-      res.status(400);
-      res.send(err);
+      console.log(err);
     } else {
       res.status(200);
-      res.send(data);
+      res.send('deleted: ' + JSON.stringify(data));
     }
   });
 });
